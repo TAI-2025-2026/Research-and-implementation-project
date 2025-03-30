2b203890b204670e7641689a3a1c90e67fc1cb77
Kifoo <magdaa2622@gmail.com>
2025-03-30 18:35:00 +0200

    Add basic screens and prepare base for futher implementation
    Screens: Home, Game, Info
    + additional helpers

--- rps_go_beyond/app/src/main/java/com/example/rps_go_beyond/ui/theme/Color.kt
+++ rps_go_beyond/app/src/main/java/com/example/rps_go_beyond/ui/theme/Color.kt
@@ -9,3 +9,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Red
+val Red_dark = Color(0xFF720303)
+val Red_intensive = Color(0xFFF80303)
+val Red_bright = Color(0xFFF35B5B)
+
+// Blue
+val Blue_dark = Color(0xFF031B72)
+val Blue_intensive = Color(0xFF038EF8)
+val Blue_bright = Color(0xFF5BE1F3)
+
+// Green
+val Green_dark = Color(0xFF0B5603)
+val Green_intensive = Color(0xFF1CF803)
+val Green_bright = Color(0xFF9BF175)
+
+// Yellow
+val Yellow_dark = Color(0xFF9F900A)
+val Yellow_intensive = Color(0xFFF6DE09)
+val Yellow_bright = Color(0xFF5BE1F3)
